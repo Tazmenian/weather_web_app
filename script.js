a3643cc8d372180a1c37bdbc3ae2215f30a387cb
Tazmenian <Tazme343@gmail.com>
2024-02-17 22:04:21 +0800

    Update HTML , CSS, SCript ongoing BTN

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+
+    window.addEventListener('resize', () => {
+        // Check if the window width is greater than 768px
+        if (window.innerWidth < 800) {
+            sideMenu.style.display = 'block';
+         } // Show the side menu
+    });
+    
+})
+
+
+
+const themeToggler = document.querySelector(".theme-toggler");
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+})
+
