e4fc4fc26abe54de26505319ea25c35884637ad6
Tazmenian <Tazme343@gmail.com>
2024-02-25 05:37:00 +0800

    Update Script

--- weather.js
+++ weather.js
@@ -5,19 +5,20 @@ const condition = document.querySelector("#condition");
 const rain = document.querySelector("#rain");
 const mainIcon = document.querySelector("#icon");
 const uvIndex = document.querySelector(".uv-index");
-const uvText = document.querySelector(".uv-index");
-const windSpeed = document.querySelector(".uv-index");
-const sunRise = document.querySelector(".uv-index");
-const sunSet = document.querySelector(".uv-index");
-const humidity = document.querySelector(".uv-index");
-const visibility = document.querySelector(".uv-index");
-const humidityStatus = document.querySelector(".uv-index");
-const airQuality = document.querySelector(".uv-index");
-const airQualityStatus = document.querySelector(".uv-index");
-const visibilityStatus = document.querySelector(".uv-index");
+const uvText = document.querySelector(".uv-text");
+const windSpeed = document.querySelector(".wind-speed");
+const sunRise = document.querySelector(".sunrise");
+const sunSet = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const visibility = document.querySelector(".visibility");
+const humidityStatus = document.querySelector(".humidity-status");
+const airQuality = document.querySelector(".air-quality");
+const airQualityStatus = document.querySelector(".air-quality-status");
+const visibilityStatus = document.querySelector(".visibility-status");
+
 
 let currentCity = "";
-let currentUnit = "";
+let currentUnit = "c";
 let hourlyorWeek = "Week";
 
 // Time Update
@@ -71,6 +72,7 @@ function getPublicIp() {
     .then((data) => {
         console.log(data);
         currentCity = data.currentCity;
+        getWeatherData(data.city, currentUnit, hourlyorWeek);
     })
 }
 
@@ -91,14 +93,27 @@ function getWeatherData(city, unit, hourlyorWeek) {
     .then((response) => response.json())
     .then((data) => {
         let today = data.currentConditions;
-        if (unit === "c") {
-            temp.innerText = today.temp;
-        } else {
-            temp.innerText = celsiusToFahrenheit(today.temp);
+        if (temp) {
+            if (unit === "c") {
+                temp.innerText = today.temp;
+            } else {
+                temp.innerText = celsiusToFahrenheit(today.temp);
+            }
         }
         currentLocation.innerText = data.resolvedAddress;
-        CSSConditionRule.innerText = today.conditions;
+        condition.innerText = today.conditions;
         rain.innerText = "Perc -" + today.precip + "%";
+        uvIndex.innerText = today.uvindex;
+        windSpeed.innerText = today.windspeed;
+        humidity.innerText = today.humidity + "%";
+        visibility.innerText = today.visibility;
+        airQuality.innerText = today.winddir;
+        measureUvindex(today.uvindex);
+        updateHumidityStatus(today.humidity);
+        updateVisibilityStatus(today.visibility);
+        updateAirQualityStatus(today.winddir);
+        sunRise.innerText = convertTimeTo12HourFormat(today.sunrise);
+        sunSet.innerText = convertTimeTo12HourFormat(today.sunset);
     });
 }
 
@@ -107,4 +122,76 @@ function celciusToFahrenheit(temp) {
     return ((temp * 9) / 5 + 32).toFixed(1);
 }
 
+//function to get the UV index status
 
+function measureUvindex(uvIndex) {
+    if (uvIndex <= 2 ){
+        uvText.innerText = "Low";
+    } else if (uvText <= 5) {
+        uvText.innerText = "Moderate";
+    } else if (uvText <= 7) {
+        uvText.innerText = "High";
+    } else if (uvText <= 10) {
+        uvText.innerText = "Very High";
+    } else {
+        uvText.innerText = "Extreme";
+    }
+}
+//function to get the Humidity status
+function updateHumidityStatus(humidity) {
+    if (humidity <= 30) {
+        humidityStatus.innerText = "Low";
+    } else if (humidity <= 60) {
+        humidityStatus.innerText = "Moderate";
+    } else {
+        humidityStatus.innerText = "High"
+    }
+}
+//function to get the Visibility status
+function updateVisibilityStatus(visibility) {
+    if (visibility <= 0.3){
+        visibilityStatus.innerText = "Dense Fog";
+    } else if (visibility <= 0.16) {
+        visibilityStatus.innerText = "Moderate Fog";
+    } else if (visibility <= 0.35) {
+        visibilityStatus.innerText = "Light Fog";
+    } else if (visibility <= 1.13) {
+        visibilityStatus.innerText = "Very  Light Fog";
+    } else if (visibility <= 2.16) {
+        visibilityStatus.innerText = "Light Mist";
+    } else if (visibility <= 5.4) {
+        visibilityStatus.innerText = "Very Light Mist";
+    } else if (visibility <= 10.8) {
+        visibilityStatus.innerText = "Clear Air";
+    } else {
+        visibilityStatus.innerText = "Very Clear Air";
+    }
+}
+//function to get the Air Quality status
+function updateAirQualityStatus(airQuality) {
+    if (airQuality <= 50) {
+        airQualityStatus.innerText = "Good";
+    } else if (airQuality <= 100) {
+        airQualityStatus.innerText = "Moderate";
+    } else if (airQuality <= 150) {
+        airQualityStatus.innerText = "Unhealthy for Sensitive Groups";
+    } else if (airQuality <= 200) {
+        airQualityStatus.innerText = "Unhealthy";
+    } else if (airQuality <= 250) {
+        airQualityStatus.innerText = "Very Unhealthy";
+    } else{
+        airQualityStatus.innerText = "Hazardous";
+    }
+}
+
+function convertTimeTo12HourFormat(time) {
+    let hour = time.split(":")[0];
+    let minute = time.split(":")[1];
+    let ampm = hour >= 12 ? "PM" : "AM";
+    hour = hour & 12;
+    hour = hour ? hour : 12;
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    let strTime = hour + ":" + minute + ":" + ampm;
+    return strTime;
+}
